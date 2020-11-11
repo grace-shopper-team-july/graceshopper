a7e8cc17346240fe5650c8ac4cf7217ae7484934
Jasmine Hatcher <jasminehatchernyc@gmail.com>
2020-11-11 15:23:32 -0500

    reducer for single product view

---
+++ client/store/singleProduct.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+//Action Types
+const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
+
+//Action Creators
+const getSingleProduct = product => {
+  return {
+    type: GET_SINGLE_PRODUCT,
+    product
+  }
+}
+
+//Thunk
+const fetchSingleProduct = id => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`api/products/${id}`)
+      dispatch(getSingleProduct(data))
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+}
+
+//Initial State
+const initialState = {
+  singleProduct: {}
+}
+
+//Reducer
+const singleProductReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SINGLE_PRODUCT:
+      return {...state, singleProduct: action.product}
+    default:
+      return state
+  }
+}
