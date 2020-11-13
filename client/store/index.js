e1923b6c3617d99a0384824abb8d2b55a5fa1491
Samantha Ernano <samantha.ernano@gmail.com>
2020-11-13 13:58:30 -0500

    Added cart to redux state

--- client/store/index.js
+++ client/store/index.js
@@ -5,12 +5,13 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import singleProduct from './singleProduct'
 import productsReducer from './products'
+import cartReducer from './cart'
 
 const reducer = combineReducers({
   user,
   productsReducer,
-  singleProduct
-
+  singleProduct,
+  cartReducer
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
