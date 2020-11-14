dab41cfd5cfd542bdc5224adf9375512bb07a32f
Jasmine Hatcher <jasminehatchernyc@gmail.com>
2020-11-13 21:57:31 -0500

    manage all products page displayed

--- client/components/Admin_ManageProducts.js
+++ client/components/Admin_ManageProducts.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
 
-const ManageProducts = props => {
-  return <div>This is the Manage Products Page..</div>
+class ManageProducts extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+
+  render() {
+    console.log(this.props)
+    const productsArr = this.props.products
+    productsArr.sort((a, b) => a.id - b.id)
+    return (
+      <div>
+        {productsArr && productsArr.length > 0 ? (
+          productsArr.map(product => {
+            return (
+              <div key={product.id}>
+                <h4>Product Id: {product.id}</h4>
+                <h4>Product Name: {product.name}</h4>
+                <button>Edit</button>
+                <button>Remove</button>
+              </div>
+            )
+          })
+        ) : (
+          <div>...Loading...</div>
+        )}
+      </div>
+    )
+  }
 }
 
-export default ManageProducts
+const mapState = state => {
+  return {
+    products: state.productsReducer.products
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    fetchProducts: () => dispatch(fetchProducts())
+  }
+}
+
+export default connect(mapState, mapDispatch)(ManageProducts)
