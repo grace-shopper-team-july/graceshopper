92a5745724c72001861c1e9ad17d8137cb3d72e1
Samantha Ernano <samantha.ernano@gmail.com>
2020-11-14 10:07:03 -0500

    Rewrote shopping cart functions to integrate with Redux state

--- client/shopping-cart-functions.js
+++ client/shopping-cart-functions.js
@@ -11,40 +11,35 @@ export function setShoppingCart(products) {
   localStorage.setItem('cart', JSON.stringify(products))
 }
 
-export function updateShoppingCart(cartUpdateFunction) {
-  let newCart = cartUpdateFunction(getShoppingCart())
+export function addProductToCart(product, qty) {
+  let newCart = getShoppingCart()
+  for (let i = 0; i < newCart.length; i++) {
+    if (newCart[i].id === product.id) {
+      newCart[i].qty += qty
+      setShoppingCart(newCart)
+      return newCart
+    }
+  }
+  newCart.push(product)
   setShoppingCart(newCart)
   return newCart
 }
 
-export function addProductToCart(product, qty) {
-  updateShoppingCart(cart => {
-    for (let i = 0; i < cart.length; i++) {
-      if (cart[i].id === product.id) {
-        cart[i].qty += qty
-        return cart
-      }
-    }
-    cart.push(product)
-    return cart
-  })
-}
-
 export function removeProductFromCart(productId) {
-  updateShoppingCart(cart => {
-    return cart.filter(item => {
-      return item.id !== productId
-    })
+  let newCart = getShoppingCart().filter(item => {
+    return item.id !== productId
   })
+  setShoppingCart(newCart)
+  return getShoppingCart()
 }
 
 export function updateProductQty(productId, qty) {
-  updateShoppingCart(cart => {
-    return cart.map(item => {
-      if (item.id === productId) {
-        item.qty = qty
-      }
-      return item
-    })
+  let newCart = getShoppingCart().map(item => {
+    if (item.id === productId) {
+      item.qty = qty
+    }
+    return item
   })
+  setShoppingCart(newCart)
+  return newCart
 }
