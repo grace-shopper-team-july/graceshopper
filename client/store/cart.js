a3f73068ad5fce362a7f16eb8b115943f0ecd851
Jasmine Hatcher <jasminehatchernyc@gmail.com>
2020-11-14 21:40:10 -0500

    working on moving cart from local to database

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import {
   getShoppingCart,
   removeProductFromCart,
@@ -7,6 +8,7 @@ import {
 
 //Action Types
 const SET_CART = 'SET_CART'
+const GET_ACTIVE_CART_ORDER = 'GET_ACTIVE_CART_ORDER'
 
 //Action Creators
 const setCart = cart => {
@@ -16,9 +18,17 @@ const setCart = cart => {
   }
 }
 
+const getActiveCartOrder = order => {
+  return {
+    type: GET_ACTIVE_CART_ORDER,
+    order
+  }
+}
+
 //Thunk
 export const fetchCart = () => {
   return dispatch => {
+    console.log('EMPTY CART GET IT FROM LOCAL STORAGE')
     const cart = getShoppingCart()
     dispatch(setCart(cart))
   }
@@ -45,6 +55,26 @@ export const updateItemQty = (item, qty) => {
   }
 }
 
+export const fetchActiveCartOrder = userId => {
+  return async dispatch => {
+    try {
+      let {data} = await axios.get('/api/orders')
+      let active = data.filter(order => {
+        return order.active === true && order.userId === userId
+      })[0]
+      if (active.length === 0) {
+        const {data} = await axios.post(`/api/orders`, userId)
+        active = data
+      }
+      const order = await axios.get(`/api/orders/${active.id}`)
+      console.log('ACTIVE ORDER', order.data)
+      dispatch(getActiveCartOrder(order.data))
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+}
+
 // Experiment with this later?
 /*export const setThunkFactory = (cartFunction) => {
   return (...params) => {
@@ -62,7 +92,8 @@ export const newFetchCart = setThunkFactory(getShoppingCart)
 
 //Initial State
 const initialState = {
-  cart: []
+  cart: [],
+  orderId: 0
 }
 
 //Reducer
@@ -70,6 +101,17 @@ export default function cartReducer(state = initialState, action) {
   switch (action.type) {
     case SET_CART:
       return {...state, cart: action.cart}
+    case GET_ACTIVE_CART_ORDER:
+      let lineItems = action.order.products.map(prd => {
+        return {
+          id: prd.id,
+          imageUrl: prd.imageUrl,
+          name: prd.name,
+          price: prd.orderLineItem.price,
+          qty: prd.orderLineItem.quantity
+        }
+      })
+      return {...state, orderId: action.order, cart: lineItems}
     default:
       return state
   }
