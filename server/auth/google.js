b49c79b78b04574ccec11f94b3ffd115d9d9ba05
Anna Szymula <ania.szymula505@gmail.com>
2020-11-18 10:00:48 -0600

    quick fix google oauth

--- server/auth/google.js
+++ server/auth/google.js
@@ -32,16 +32,13 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
     (token, refreshToken, profile, done) => {
       const googleId = profile.id
       const email = profile.emails[0].value
-      // let imgUrl = profile.photos[0].value //not needed
       const firstName = profile.name.givenName
       const lastName = profile.name.familyName
-      // let fullName = profile.displayName //not needed
-      let addressLine1 = '(empty)'
-      let city = '(empty)'
-      let state = '(empty)'
-      let zip = '(empty)'
-      // let phone = '(empty)'
-      let phone = profile.phone
+      let addressLine1 = '---'
+      let city = '---'
+      let state = '---'
+      let zip = '---'
+      let phone = '---'
 
       User.findOrCreate({
         where: {googleId},
