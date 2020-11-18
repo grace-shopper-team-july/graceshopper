0948f6497b709b19986a057d1eef0acb400cf1d7
Anna Szymula <ania.szymula505@gmail.com>
2020-11-18 05:34:11 -0600

    auth check on delete, post, & put products was successful

--- server/api/products.js
+++ server/api/products.js
@@ -44,122 +44,55 @@ router.get('/:productId', async (req, res, next) => {
 
 router.put('/:productId', async (req, res, next) => {
   try {
-    const change = await Product.update(req.body, {
-      where: {
-        id: req.params.productId
-      }
-    })
-    const updatedProduct = await Product.findOne({
-      where: {
-        id: req.params.productId
-      }
-    })
-    res.json(updatedProduct)
+    if ((await isAdmin(req)) === true) {
+      const change = await Product.update(req.body, {
+        where: {
+          id: req.params.productId
+        }
+      })
+      const updatedProduct = await Product.findOne({
+        where: {
+          id: req.params.productId
+        }
+      })
+      res.json(updatedProduct)
+    } else {
+      alert('User not Authorized for this action!')
+    }
   } catch (err) {
     next(err)
   }
 })
 
-// router.delete('/:productId', async (req, res, next) => {
-//   console.log('1')
-//   try {
-//     console.log('2')
-//     const deletedProduct = await Product.destroy({
-//       where: {
-//         id: req.params.productId
-//       }
-//     })
-//     console.log('3')
-//     if (deletedProduct === 0) {
-//       res.status(404).send('404 Error')
-//     } else {
-//       res.status(204).send()
-//     }
-//     console.log('4')
-//   } catch (err) {
-//     next(err)
-//   }
-// })
 router.delete('/:productId', async (req, res, next) => {
-  console.log('1')
   try {
-    console.log('2')
-    let authCheck = await isAdmin(req)
-    console.log(authCheck)
-    if (authCheck === true) {
-      console.log('3')
+    if ((await isAdmin(req)) === true) {
       const deletedProduct = await Product.destroy({
         where: {
           id: req.params.productId
         }
       })
-      console.log('4')
       if (deletedProduct === 0) {
         res.status(404).send('404 Error')
       } else {
         res.status(204).send()
       }
     } else {
-      console.log('isAdmin returned false??')
+      alert('User not Authorized for this action!')
     }
   } catch (err) {
     next(err)
   }
 })
-// router.delete('/:productId', async (req, res, next) => {
-//   if (isAdmin() === true) {
-//     try {
-//       const deletedProduct = await Product.destroy({
-//         where: {
-//           id: req.params.productId
-//         }
-//       })
-//       if (deletedProduct === 0) {
-//         res.status(404).send('404 Error')
-//       } else {
-//         res.status(204).send()
-//       }
-//     } catch (err) {
-//       next(err)
-//     }
-//   } else {
-//     alert('User not Authorized for this action!')
-//     res.sendStatus(401)
-//   }
-// })
-// router.delete('/:productId', isAdmin, async (req, res, next) => {
-//   try {
-//     console.log('1:     in delete prod')
-//     const authCheck = await isAdmin(req)
-//     if (authCheck === true) {
-//       console.log('2A:    in if isAdmin checker')
-//       console.log('3')
-//       const deletedProduct = await Product.destroy({
-//         where: {
-//           id: req.params.productId
-//         }
-//       })
-//       console.log('4')
-//       if (deletedProduct === 0) {
-//         res.status(404).send('404 Error')
-//       } else {
-//         res.status(204).send()
-//       }
-//       console.log('5')
-//     } else {
-//       console.log('2B')
-//       alert('User not Authorized for this action!')
-//       res.sendStatus(401)
-//     }
-//   } catch (error) {
-//     next(err)
-//   }
-// })
 
 router.post('/', async (req, res, next) => {
   try {
-    const newProduct = await Product.create(req.body)
-    res.json(newProduct)
+    if ((await isAdmin(req)) === true) {
+      const newProduct = await Product.create(req.body)
+      res.json(newProduct)
+    } else {
+      alert('User not Authorized for this action!')
+    }
   } catch (err) {
     next(err)
   }
